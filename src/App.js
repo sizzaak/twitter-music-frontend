1fa93dbf6f22cbb238de28ab4bc143e464b6c1d7
sizzaak <sizzaak@cs.washington.edu>
2021-02-02 14:28:54 -0800

    basic modified tutorial website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import ReactAudioPlayer from 'react-audio-player';
 
 function App() {
   return (
@@ -7,16 +8,22 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Twitter Emotion Music
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ReactAudioPlayer
+          src="https://media.w3.org/2010/07/bunny/04-Death_Becomes_Fur.mp4"
+          controls
+        />
+        <label for="ice-cream-choice">Which trend do you want to listen to?</label>
+        <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
+
+        <datalist id="ice-cream-flavors">
+          <option value="GameStop"></option>
+          <option value="#ThankYouAaron"></option>
+          <option value="5dogs"></option>
+          <option value="Lonzo"></option>
+          <option value="Lukaku"></option>
+        </datalist>
       </header>
     </div>
   );
