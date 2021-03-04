6a09fa5fe90768da6ec7f15af7d37d2e9327b4bf
sizzaak <sizzaak@cs.washington.edu>
2021-03-03 23:32:08 -0800

    many visual changes plus link to design doc, built for server

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import logo from './logo.svg';
+import twitter_logo from './twitter_logo.svg'
 import './App.css';
 import ReactAudioPlayer from 'react-audio-player';
+//import background from "./funky_colors_bg.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h3>
           Twitter Emotion Music
+        </h3>
+        An aggregate of the emotional state of twitter
+        <p>
+          <img src={twitter_logo} className="App-logo" alt="logo" />
         </p>
         <ReactAudioPlayer
           src="http://18.188.150.96:8000/liq.ogg"
           controls
         />
-        <label for="ice-cream-choice">Which trend do you want to listen to?</label>
-        <input list="ice-cream-flavors" id="ice-cream-choice" name="ice-cream-choice" />
-
-        <datalist id="ice-cream-flavors">
-          <option value="GameStop (Test)"></option>
-          <option value="#ThankYouAaron"></option>
-          <option value="5dogs"></option>
-          <option value="Lonzo"></option>
-          <option value="Lukaku"></option>
-        </datalist>
+        <p>
+          <a className="App-button" href="https://docs.google.com/document/d/1qih2wQpwlKFNCUW1N6w4xqkPPoUdU9reQ7R2FZN28QA/edit">Learn more about our project here</a>
+        </p>
       </header>
     </div>
   );
