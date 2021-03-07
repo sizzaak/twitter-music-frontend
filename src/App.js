a5a9c002ea60e55df4eef47678bc308a738ae7c1
sizzaak <sizzaak@cs.washington.edu>
2021-03-07 15:51:37 -0800

    testing fetch from trends.json

--- src/App.js
+++ src/App.js
@@ -10,33 +10,13 @@ class App extends React.Component {
     this.state = { trends: ["trend1", "trend2", "trend3", "trend4", "trend5"]};
   }
 
-/*
-  async postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-*/
-
   componentDidMount() {
     fetch('https://courses.cs.washington.edu/courses/cse481i/21wi/Project/tweet-analysis/trends.json')
     /*fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits', {mode: 'no-cors'})*/
       .then(response => response.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({trends: data}));
     console.log("yo");
-    this.setState({trends : ["trendA", "trendB", "trendC", "trendD", "trendE"]});
+    //this.setState({trends : ["trendA", "trendB", "trendC", "trendD", "trendE"]});
   }
 
   render() {
